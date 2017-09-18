da6af7bb2ddff3d84876942e04803eabdc545d90
yangxu <yx090411@163.com>
2017-09-18 16:33:11 +0800

    update

--- MyQuartzJobs/Program.cs
+++ MyQuartzJobs/Program.cs
@@ -17,9 +17,9 @@ namespace MyQuartzJobs
             {
                 x.UseLog4Net();
                 x.Service<ServiceRunner>();
-                x.SetDescription("我的通用定时服务");
-                x.SetDisplayName("MyService");
-                x.SetServiceName("我的通用定时服务");
+                x.SetDescription("我的通用定时服务");//安装服务后，服务的描述
+                x.SetDisplayName("MyService"); //显示名称
+                x.SetServiceName("我的通用定时服务名称"); //服务名称
                 x.EnablePauseAndContinue();
             });
         }
